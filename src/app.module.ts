07180f1ddb50555ba35b76bb5c1b4637e7019f37
Ade <ade.adebolu@beamery.com>
2022-04-04 11:14:50 +0100

    updates controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HackersController } from './hackers/hackers.controller';
 import { PreysController } from './preys/preys.controller';
+import { SecondController } from './second/second.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, HackersController, PreysController],
+  controllers: [AppController, HackersController, PreysController, SecondController],
   providers: [AppService],
 })
 export class AppModule {}
