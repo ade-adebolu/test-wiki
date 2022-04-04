0453a6f4d2f079086fe247d7b42656d3e3e7e7cb
Ade <ade.adebolu@beamery.com>
2022-04-04 15:34:55 +0100

    removed deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HackersController } from './hackers/hackers.controller';
+import { FriendsController } from './friends/friends.controller';
 import { PreysController } from './preys/preys.controller';
 import { SecondController } from './second/second.controller';
-import { FriendsController } from './friends/friends.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, HackersController, PreysController, SecondController, FriendsController],
+  controllers: [
+    AppController,
+    PreysController,
+    SecondController,
+    FriendsController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
